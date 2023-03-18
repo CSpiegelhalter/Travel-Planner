c767eb2f7a6a425c04135e21190b67381fac1158
Brandon Ballesteros <brandonballesteros9845@gmail.com>
2023-03-18 17:08:21 +0100

    TP-11

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Image from 'next/image';
+import Link from 'next/link';
 
 function NavBar(props: any) {
 
@@ -9,18 +10,19 @@ function NavBar(props: any) {
             <header className="main-header">
                 <Image src='/Globe.png' alt="WorldGlobe or food" width={40} height={40}></Image>
                 <h2 className="header-title">Our Travel-Planner</h2>
-                <p className='home-btn'>Home</p>
+                <p className='home-btn'><Link href="./index">Home</Link></p>
                 <ul className="nav">
                     <div className='nav-list-container'>
-                    <li >Login</li>
+                    <li><Link href="./SignIn">Login</Link></li>
                     </div>
                     <div className='nav-list-container'>
-                    <li>Sign-Up</li>
+                    <li><Link href="./SignUp">Sign-Up</Link></li>
                     </div>
                 </ul>
             </header>
         </div>
     );
 }
+
 
 export default NavBar;
--- pages/signUp.tsx
+++ pages/signUp.tsx
@@ -1,19 +1,22 @@
 import React from 'react';
+import NavBar from '@/components/NavBar';
 
 function signUp(props) {
     return (
         <div>
-            <p>Welcome please fill out the information below to sign up!</p>
-            <p>Please enter your email:</p>
-            <input type='form' placeholder='Email address' />
-            <p>Please confirm your email:</p>
-            <input type='form' placeholder='Confirm email address' />
-            <p>Please enter a password </p>
-            <input type='form' placeholder='Password' />
-            <p>Please comfirm your password:</p>
-            <input type='form' placeholder='Confirm password' />
-            <button type='submit'>Submit</button>
-
+            <NavBar />
+            <div className='signUp container'>
+                <p>Welcome please fill out the information below to sign up!</p>
+                <p>Please enter your email:</p>
+                <input type='form' placeholder='Email address' />
+                <p>Please confirm your email:</p>
+                <input type='form' placeholder='Confirm email address' />
+                <p>Please enter a password </p>
+                <input type='form' placeholder='Password' />
+                <p>Please comfirm your password:</p>
+                <input type='form' placeholder='Confirm password' />
+                <button type='submit'>Submit</button>
+            </div>
 
         </div>
     );
