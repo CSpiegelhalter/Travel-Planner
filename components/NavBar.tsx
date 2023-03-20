592d3690c69a2792f6cd5d150381e73db02c49fa
Brandon Ballesteros <brandonballesteros9845@gmail.com>
2023-03-20 15:19:20 +0100

    TP-11

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -10,13 +10,13 @@ function NavBar(props: any) {
             <header className="main-header">
                 <Image src='/Globe.png' alt="WorldGlobe or food" width={40} height={40}></Image>
                 <h2 className="header-title">Our Travel-Planner</h2>
-                <p className='home-btn'><Link href="./index">Home</Link></p>
+                <p className='home-btn'><Link href="/">Home</Link></p>
                 <ul className="nav">
                     <div className='nav-list-container'>
-                    <li><Link href="./SignIn">Login</Link></li>
+                    <li><Link href="/signIn">Login</Link></li>
                     </div>
                     <div className='nav-list-container'>
-                    <li><Link href="./SignUp">Sign-Up</Link></li>
+                    <li><Link href="/signUp">Sign-Up</Link></li>
                     </div>
                 </ul>
             </header>
