df9a91ee1237e7fc8ae8e228140dfee84d925be2
Brandon Ballesteros <brandonballesteros9845@gmail.com>
2023-03-28 21:28:14 +0100

    TP-16

---
+++ constants/constants.ts
@@ -0,0 +1,67 @@
+// enum Direction {
+//     Up,
+//     Down,
+//     Left,
+//     Right
+//   }
+
+export enum Shopping {
+    clothing_store,
+    department_store,
+    electronics_store,
+    shoe_store,
+    shopping_mall
+}
+
+export enum Resturants {
+    cafe,
+    meal_delivery,
+    meal_takeaway,
+    restaurant
+}
+
+export enum Bars{
+    bar,
+    night_club
+}
+
+export enum Lodging{
+    lodging,
+    campground,
+    rv_park
+}
+
+export enum Attractions {
+    aquarium,
+    amusement_park,
+    art_gallery,
+    campground,
+    casino,
+    movie_theater,
+    museum,
+    park,
+    tourist_attraction,
+    zoo
+}
+
+export enum Transportation{
+    airport,
+    bus_station,
+    car_rental,
+    taxi_stand,
+    train_station,
+    travel_agency
+}
+
+export enum Grocery{
+    bakery,
+    convenience_store,
+    liquor_store,
+    store,
+    supermarket
+}
+
+export enum Financial{
+    bank,
+    atm
+}
