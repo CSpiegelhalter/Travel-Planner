418f72b7b824749caa2e824f54cd8785646bfaca
Brandon Ballesteros <brandonballesteros9845@gmail.com>
2023-04-03 14:59:23 +0100

    TP-14

--- pages/api/pointsOfInterest.ts
+++ pages/api/pointsOfInterest.ts
@@ -1,17 +1,30 @@
 import { locationLabels } from "@/constants/constants"
 
-const runThroughKeys = async () => {
-    for(let key of Object.keys(locationLabels)){
-        console.log(key)
-    }
-    return "I have been returned"
-}
 
 export default async function handler(req: any, res: any){
     const body = JSON.parse(req.body)
     let city = body['city']
     let pointOfInterest = body['point']
     
+    const runThroughKeys = async () => {
+        let keyArr: string[] = []
+        for(let key of Object.keys(locationLabels)){
+            if (key === pointOfInterest ){
+            keyArr.push(locationLabels[key])        
+            }
+        }
+        keyArr = keyArr.flat()
+        
+         let promiseArr = []
+         for (let i of keyArr){
+            promiseArr.push(fetch(`https://maps.googleapis.com/maps/api/place/textsearch/json?query=${i}{+in+${city}&key=${process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY}`).then(function(response){
+                return response.json()
+            }))
+         }
+        return Promise.all(promiseArr)
+    }
+    
+
     console.log( await runThroughKeys())
     const placesApi = `https://maps.googleapis.com/maps/api/place/textsearch/json?query=${pointOfInterest}{+in+${city}&key=${process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY}`
     const results = await fetch(placesApi)
--- pages/index.tsx
+++ pages/index.tsx
@@ -9,11 +9,10 @@ import Map from '@/components/Map'
 import Button from '@/components/Button'
 import SideBar from '@/components/SideBar'
 import PlaceComponent from '@/components/PlaceComponent'
+import { locationLabels } from '@/constants/constants'
 
 
 export default function Home() {
-
-  const pointsOfInterest: string[] = [ 'restaurant', 'shopping', 'bar']
 
   //These are the two states used to get our location for centering
   const [location, setLocation] = useState({lat: 51.5072, lng:0.1276 })
@@ -87,7 +86,7 @@ export default function Home() {
         <div className='info-container'>
           <div className='pointsOfInterest-filter-container' >
             <div className="pointsOfInterest-btn-container">
-            {pointsOfInterest.map((point, index) => <Button key={index} name='here' handler={callApi} value={point} />)}
+            {Object.keys(locationLabels).map((point, index) => <Button key={index} name='here' handler={callApi} value={point} />)}
             </div>
             <PlaceComponent />
            </div>
