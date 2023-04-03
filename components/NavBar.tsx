70fc601aa6222d32804a97d815b52a7a7e739a8e
Brandon Ballesteros <brandonballesteros9845@gmail.com>
2023-04-03 19:33:37 +0100

    TP-17

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -11,6 +11,8 @@ function NavBar(props: any) {
                 <Image src='/Globe.png' alt="WorldGlobe or food" width={40} height={40}></Image>
                 <h2 className="header-title">Our Travel-Planner</h2>
                 <p className='home-btn'><Link href="/">Home</Link></p>
+            
+                <p className='profile-btn'><Link href="/profilePage"> Profile</Link></p>
                 <ul className="nav">
                     <div className='nav-list-container'>
                     <li><Link href="/signIn">Login</Link></li>
---
+++ pages/profilePage.tsx
@@ -0,0 +1,21 @@
+import NavBar from '@/components/NavBar';
+import React from 'react';
+
+function profilePage(props) {
+    return (
+        <>
+            <NavBar />
+            <div className='profile-page-container'>
+                <div className='profile-container'>
+                    <div className="test">test</div>
+                    <div className="test">test</div>
+                </div>
+                <div>
+                    
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default profilePage;
