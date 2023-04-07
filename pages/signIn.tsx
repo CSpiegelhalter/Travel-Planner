62096377d931f9b8d81993f4a9abaa260541d6a8
Brandon Ballesteros <brandonballesteros9845@gmail.com>
2023-04-07 11:08:41 +0100

    TP-20

--- pages/signIn.tsx
+++ pages/signIn.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
+import { useState } from 'react'
 import NavBar from '@/components/NavBar'
+
 
 const callSignInApi = async () => {
   const params = {
@@ -14,19 +16,31 @@ const callSignInApi = async () => {
   console.log(await data.json())
 }
 
-
 function signUp(props) {
+  const [email, setEmail] = useState()
+  const [password, setPassword] = useState()
+
+function handleEmailInput(event){  
+  setEmail(event.target.value)
+
+}
+
+function handlePasswordInput(event){
+  setPassword(event.target.value)
+}
+
+const bleh = () => {console.log(email, password)}
   return (
     <div>
       <NavBar />
       <div className="signIn-container">
         <p>Enter your email:</p>
-        <input type="form" placeholder="Email address" />
+        <input type="form" placeholder="Email address" onChange={handleEmailInput}/>
         <p>Enter your password:</p>
-        <input type="form" placeholder="password" />
+        <input type="password" placeholder="Password" onChange={handlePasswordInput}/>
         <button type="submit">Submit</button>
       </div>
-      <button onClick={callSignInApi}>test</button>
+      <button onClick={bleh}>test</button>
     </div>
   )
 }
