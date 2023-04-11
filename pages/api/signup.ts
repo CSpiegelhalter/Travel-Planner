a9605a2b35b0bfcc1a05f98d3ed89519b33cd0af
Brandon Ballesteros <brandonballesteros9845@gmail.com>
2023-04-11 21:45:38 +0100

    saving for stash

--- pages/api/signup.ts
+++ pages/api/signup.ts
@@ -1,6 +1,19 @@
 export default async function handler(req: any, res: any) {
-  const data = await fetch(process.env.NEXT_PUBLIC_AWS_API_SIGNUP_LINK as any)
 
-  console.log(await data.json())
+  console.log(req.body)
+  const options = {
+    method: 'POST',
+    body: req.body,
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    },
+  }
+
+  const data = await fetch(process.env.NEXT_PUBLIC_AWS_API_SIGNUP_LINK as any, options)
+
+  // const apiReponseData = await data.json()
+  console.log(await data.text())
+  // res.send('whoot')
   await res.status(200).send(JSON.stringify(data))
 }
