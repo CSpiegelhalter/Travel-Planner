94a5623090fc6c3cadf9e0e74ce975dc5e4be2a8
Brandon Ballesteros <brandonballesteros9845@gmail.com>
2023-04-12 20:33:12 +0100

    ahhh;

--- pages/signUp.tsx
+++ pages/signUp.tsx
@@ -32,10 +32,11 @@ function signUp(props) {
 
     const data = await fetch('/api/signup', options)
     //converts the ReadableStream into a string
-    const dataCheck= await data.json()
+    const dataCheck = await data.json()
     //decides what to do depending on the data returned, either redirect or tells alert state what to display
     if (dataCheck.success) {
-      router.push('/')    }
+      router.push('/')
+    }
     else if (dataCheck.error === "User already exists!") {
       setAlert("User already exists!")
     }
@@ -46,7 +47,7 @@ function signUp(props) {
 
 
   //This checks if the email/pass and confirm match
-  const validateInput  = () => {
+  const validateInput = () => {
     //this is used to reset the alert to falsy on the first confirm check
     if (alert) {
       setAlert('')
