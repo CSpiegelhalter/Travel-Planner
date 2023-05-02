8f3a942971ff63e8d398235c56d4213d7ba5e7e5
Brandon Ballesteros <brandonballesteros9845@gmail.com>
2023-05-02 20:44:29 +0100

    refactored navbar css

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -3,28 +3,28 @@ import Image from 'next/image'
 import Link from 'next/link'
 import Dropdown from './Dropdown'
 import { test } from 'node:test'
-
+import styles from '../styles/Modal.module.css'
 
 function NavBar(props: any) {
   const testList = ['spain', 'rome', 'vacay', 'our getaway']
 
   return (
-    <div className="nav-container">
-      <header className="main-header">
+    <div className={styles.navContainer}>
+      <header className={styles.mainHeader}>
         {/* <Image src="/Globe.png" alt="WorldGlobe or food" width={40} height={40}></Image> */}
         <Dropdown tripsList={testList}/> 
-        <h2 className="header-title">Our Travel-Planner</h2>
+        <h2 className={styles.headerTitle}>Our Travel-Planner</h2>
        {props.user &&  <p>Hello, {props.user.name}</p>}
-        <p className="home-btn">
-          <Link href="/home">Home</Link>
+        <p className={styles.homeBtn}>
+          <Link href={styles.home}>Home</Link>
         </p>
 
-        <p className="profile-btn">
+        <p className={styles.profileBtn}>
           <Link href="/profilePage"> Profile</Link>
         </p>
         {!props.user &&
-          <ul className="nav">
-            <div className="nav-list-container">
+          <ul className={styles.nav}>
+            <div className={styles.navListContainer}>
               <li>
                 <Link href="/api/auth/login">Login/Signup</Link>
               </li>
@@ -32,9 +32,9 @@ function NavBar(props: any) {
           </ul>
         }
         {props.user &&
-          <div className="nav-list-container">
+          <div className="navListContainer">
             <ul>
-              <li className= "nav-list-item"><Link href="/api/auth/logout">logout</Link></li>
+              <li className= "navListItem"><Link href="/api/auth/logout">logout</Link></li>
             </ul>
           </div>
         }
