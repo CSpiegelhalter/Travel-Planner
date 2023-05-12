fdd4861d79dbe170efecb16a9577225e39f923e7
CSpiegelhalter <spiegelhaltercurt@gmail.com>
2023-05-12 13:44:35 -0500

    Finished landing page (1st iteration)

--- components/LandingComponent.tsx
+++ components/LandingComponent.tsx
@@ -3,11 +3,9 @@
 import React from 'react'
 import styles from '../styles/componentStyles/LandingComponent.module.css'
 import Button from './Button'
-import { useRouter } from 'next/router'
 import Hamburger from './Hamburger'
 
-function LandingComponent({ darkmode }) {
-  const router = useRouter()
+function LandingComponent({ darkmode, router }) {
 
   return (
     <>
@@ -16,7 +14,7 @@ function LandingComponent({ darkmode }) {
     </div>
       <div className={styles.background}>
         <p className={styles.title}>A tool to help you find fun and unique things to do on your travels.</p>
-        <Button style={darkmode ? {background: '#19376d', border: '1px solid #060047'} : {background: '#E64089', border: '1px solid #051c32'}}  name="landingPageFindPlaces" value="Find Places >>" handler={() => router.push('/home')} />
+        <Button style={darkmode ? {background: '#19376d', border: '1px solid #141e61', boxShadow: '4px 4px 0px #051c32'} : {background: '#E64089', border: '1px solid #060047', boxShadow: '4px 4px 0px #060047' }}  name="landingPageFindPlaces" value="Find Places >>" handler={() => router.push('/home')} />
       </div>
     </>
   )
