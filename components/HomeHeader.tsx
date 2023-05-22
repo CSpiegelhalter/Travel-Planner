b0ac49c752bb9a9cea4049d083856f000eeafe1c
Brandon Ballesteros <brandonballesteros9845@gmail.com>
2023-05-22 16:02:22 +0100

    fixes

--- components/HomeHeader.tsx
+++ components/HomeHeader.tsx
@@ -19,14 +19,14 @@ function HomeHeader(props: HomeHeader) {
         <header className={styles.mainHeader}>
             <div className={styles.centerTop}>
                 <div className={styles.placeContainer}>
-                    <PlaceComponent isLoaded={props.isLoaded} style={{paddingRight:'0', marginRight:'0'}} />
+                    <PlaceComponent isLoaded={props.isLoaded} style={{ paddingRight: '0', marginRight: '0' }} />
                     <div className={styles.hamburgerContainer}>
-                    <Hamburger className={styles.hamburger} style={{width:'4rem',marginTop: '1rem',marginBottom: '0.5rem', borderRadius:'3rem', background:'white', paddingTop:'0.3rem', marginRight:'1rem' }} user={props.user} />
+                        <Hamburger className={styles.hamburger} style={{ width: '4rem', marginTop: '1rem', marginBottom: '0.5rem', borderRadius: '3rem', background: 'white', paddingTop: '0.3rem', marginRight: '1rem' }} user={props.user} />
                     </div>
                 </div>
                 <div className={styles.btnContainer}>
                     {Object.keys(locationLabels).map((point, index) => (
-                        <Button key={index} name="attractionsFilterBtn" handler={callPointsOfInterestsApi} value={point} params={{ typeOfInterest: point, ...params }} disabled="" style={{padding:'0.3rem', height:'2rem',fontWeight:'700' }} />
+                        <Button key={index} name="attractionsFilterBtn" handler={callPointsOfInterestsApi} value={point} params={{ typeOfInterest: point, ...params }} disabled="" style={{ padding: '0.3rem', height: '2rem', fontWeight: '700' }} />
                     ))}
                 </div>
             </div>
