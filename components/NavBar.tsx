51e480be8ce646772eaf28b73d7b964ff80ccc6c
Brandon Ballesteros <brandonballesteros9845@gmail.com>
2023-05-22 17:06:32 +0100

    replacing svgs with webp

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link'
 import styles from '../styles/componentStyles/NavBar.module.css'
 import Image from 'next/image'
 
@@ -8,22 +9,32 @@ function NavBar(props: any) {
     <header className={styles.mainHeader}>
       <div className={styles.buttonContainer}>
         <div className={styles.mapBtn}>
+          <Link href='/mapTool' prefetch={false}>
           <Image src="/mapIconSelected.webp" alt="Backpack Icon" width={50} height={50} />
           <p className={styles.btnLabels}>Maps</p>
+          </Link>
         </div>
         <div className={styles.tripsBtn}>
+          <Link href='/trips' prefetch={false}>
           <Image src="/backpackIconSelected.webp" alt="Backpack Icon" width={50} height={50} />
           <p className={styles.btnLabels}>Trips</p>
+          </Link>
         </div>
         <div className={styles.bucketListBtn}>
+          <Link href='/bucketList' prefetch={false}>
           <Image src="/saveIconSelected.webp" alt="Backpack Icon" width={50} height={50} />
-          <p className={styles.btnLabels}>Saved</p>
+          <div className={styles.bucketLabel}>
+          <p className={styles.btnLabels}>Bucket List</p>
+          </div>
+          </Link>
         </div>
         <div className={styles.ProfileBtn}>
+          <Link href='/profilePage' prefetch={false}>
           <Image src="/backpackIconSelected.webp" alt="Backpack Icon" width={50} height={50} />
           <div className={styles.meLabel}>
             <p className={styles.btnLabels}>Me</p>
           </div>
+            </Link>
         </div>
       </div>
     </header>
