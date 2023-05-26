028c65d4619b8ea8af1a3b42c70154701b207317
Brandon Ballesteros <brandonballesteros9845@gmail.com>
2023-05-26 17:41:13 +0100

    fixes

---
+++ components/ProfileComponent.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import styles from '@/styles/pageStyles/Profile.module.css'
+import Link from 'next/link'
+
+
+function ProfileComponent(props: any) {
+    return (
+        <div className={styles.profileContainer}>
+        <h1>{`${props.user?.given_name} ${props.user?.family_name}`}</h1>
+        <Link href='/trips' prefetch={false} className={styles.link}>
+        <p className={styles.savedTrips}>{`${props.trips.length} Trips`}</p>
+        </Link>
+        <div className={styles.profileItemsList}>
+          <ul className={styles.profileList}> 
+            <li className={styles.listItem}>{props.user?.given_name as string}</li>
+            <li className={styles.listItem}>{props.user?.family_name as string}</li>
+            <li className={styles.listItem}>{props.user?.nickname}</li>
+            <li className={styles.listItem}>{props.user?.email}</li>
+          </ul>
+        </div>
+        <Link className={styles.logout} href="api/auth/logout" prefetch={false}>Logout</Link>
+      </div>
+    );
+}
+
+export default ProfileComponent;
--- pages/profile.tsx
+++ pages/profile.tsx
@@ -1,10 +1,11 @@
 import { useUser } from '@auth0/nextjs-auth0/client'
-import Link from 'next/link'
 import styles from '@/styles/pageStyles/Profile.module.css'
 import dynamic from 'next/dynamic'
 
 function profilePage() {
   const NavBar = dynamic(() => import('@/components/NavBar'))
+  const ProfileComponent = dynamic(() => import('@/components/ProfileComponent'))
+  const ProfileDefault = dynamic(() => import('@/components/ProfileDefault'))
   const { user } = useUser()
   const userId: number = user?.['https://example.com/id'] as number
   const profilepicture = user?.picture
@@ -14,19 +15,12 @@ function profilePage() {
   return (
     <div className={styles.mainContainer}>
       <NavBar profile={true} bucketList={false} trips={false} map={false} />
-      <div className={styles.profileContainer}>
-        <h1>{`${user?.given_name} ${user?.family_name}`}</h1>
-        <p className={styles.savedTrips}>{`${trips.length} Trips`}</p>
-        <div className={styles.profileItemsList}>
-          <ul className={styles.profileList}> 
-            <li className={styles.listItem}>{user?.given_name as string}</li>
-            <li className={styles.listItem}>{user?.family_name as string}</li>
-            <li className={styles.listItem}>{user?.nickname}</li>
-            <li className={styles.listItem}>{user?.email}</li>
-          </ul>
-        </div>
-        <Link className={styles.logout} href="api/auth/logout" prefetch={false}>Logout</Link>
-      </div>
+      {user ? 
+      <ProfileComponent  user={user} trips={trips}/>
+      :
+      <ProfileDefault /> 
+ 
+    }
     </div>
   )
 }
