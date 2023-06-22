f7c226c040fa79670740f97324aece6fa9bee978
Brandon Ballesteros <brandonballesteros9845@gmail.com>
2023-06-22 19:28:55 +0100

    maybe done

--- pages/bucketList.tsx
+++ pages/bucketList.tsx
@@ -6,6 +6,7 @@ import { locationObj } from '@/Types/types'
 import ProfileDefault from '@/components/Profilepage/ProfileDefault'
 import { useUser } from '@auth0/nextjs-auth0/client'
 import { grabDataToDisplay } from '@/helperFunctions/grabDataToDisplay'
+import LoadingPage from '@/components/LoadingPage'
 
 function bucketList(props: any) {
   const NavBar = dynamic(() => import('@/components/NavBar'))
@@ -15,16 +16,29 @@ function bucketList(props: any) {
   const [locationData, setLocationData] = useState<locationObj[]>([])
   const [display, setDisplay] = useState(false)
   const testData = testLocations
-  const { user } = useUser()
+  const { user, isLoading } = useUser()
   const userId: number | any = process.env.AUTH0_USER_ID ? user?.[process.env.AUTH0_USER_ID] : null
 
   useEffect(() => {
     if (!!user) {
-      grabDataToDisplay(userId, 'bucketList').then((val) => {
+      grabDataToDisplay(userId, 'trips').then((val) => {
         setLocationData(val)
       })
+      if (Object.keys(locationData).length > 0) {
+        setDisplay(true)
+      }
     }
-  }, [])
+  }, [isLoading, user])
+
+  //I AM FOR TESTING!
+  // useEffect(() => {
+  //   setLocationData(testData)
+  //   setDisplay(true)
+  // })
+
+  if(isLoading){
+     <LoadingPage />
+  }
 
   return (
     <>
@@ -33,27 +47,30 @@ function bucketList(props: any) {
           <header className={styles.header}>
             <h1 className={styles.headerCaption}>My Bucketlist:</h1>
           </header>
+          <>
             {display ? (
-          <div className={styles.scrollableArea}>
-              locationData.map((location: any, index: number) => (
-                <Card
-                  name={location.name}
-                  rating={location.rating}
-                  address={location.address}
-                  lat={location.lat}
-                  lng={location.lng}
-                  attractionType={location.attractionType}
-                  reviewCount={location.reviewCount}
-                  imageUrl={location.imageUrl}
-                  descriptionShort={location.descriptionShort}
-                  descriptionLong={location.descriptionLong}
-                  hideButtons={true}
-                />
-              ))
-          </div>
+              <div className={styles.scrollableArea}>
+                {locationData.map((location: any, index: number) => (
+                  <Card
+                    name={location.name}
+                    rating={location.rating}
+                    address={location.address}
+                    lat={location.lat}
+                    lng={location.lng}
+                    attractionType={location.attractionType}
+                    reviewCount={location.reviewCount}
+                    imageUrl={location.imageUrl}
+                    descriptionShort={location.descriptionShort}
+                    descriptionLong={location.descriptionLong}
+                    hideButtons={true}
+                    key={index}
+                  />
+                ))}
+              </div>
             ) : (
               <BucketListDefault />
             )}
+          </>
         </div>
       ) : (
         <ProfileDefault />
--- pages/trips.tsx
+++ pages/trips.tsx
@@ -5,6 +5,7 @@ import { testTrips } from '@/constants/test'
 import { useUser } from '@auth0/nextjs-auth0/client'
 import ProfileDefault from '@/components/Profilepage/ProfileDefault'
 import { grabDataToDisplay } from '@/helperFunctions/grabDataToDisplay'
+import LoadingPage from '@/components/LoadingPage'
 
 function trips(props: any) {
   const TripDisplay = dynamic(() => import('@/components/Tripspage/TripDisplay'))
@@ -12,8 +13,10 @@ function trips(props: any) {
   const Modal = dynamic(() => import('@/components/Modal'))
   const TripsDefault = dynamic(() => import('@/components/Tripspage/TripsDefault'))
   const CreateTrip = dynamic(() => import('@/components/Tripspage/CreateTrip'))
-  const { user } = useUser()
-  const userId: number | any = process.env.NEXT_PUBLIC_AUTH0_USER_ID ? user?.[process.env.NEXT_PUBLIC_AUTH0_USER_ID] : null
+  const { user, isLoading } = useUser()
+  const userId: number | any = process.env.NEXT_PUBLIC_AUTH0_USER_ID
+    ? user?.[process.env.NEXT_PUBLIC_AUTH0_USER_ID]
+    : null
   // console.log(process.env.NEXT_PUBLIC_AUTH0_USER_ID)
   // const userId: number = user?.['https://example.com/id'] as number
   console.log(userId, typeof userId)
@@ -27,45 +30,48 @@ function trips(props: any) {
       grabDataToDisplay(userId, 'trips').then((val) => {
         setTripData(val)
       })
-      if(Object.keys(tripData).length > 0 ){
+      if (Object.keys(tripData).length > 0) {
         setTripDisplay(true)
       }
     }
-  }, [])
+  }, [isLoading, user])
 
+  if(isLoading){
+    return <LoadingPage />
+
+  }
 
   return (
-    <>
-      {user ? (
-        <div className={styles.mainContainer}>
-          <div className={styles.topContainer}>
-            <header className={styles.tripHeader}>
-              <p className={styles.header}>My Trips:</p>
-              <CreateTrip setModalDisplay={setModalDisplay} />
-            </header>
-            {tripDisplay ? (
-              <div className={styles.cardContainer}>
-                {Object.entries(tripData).map(([key, value]: any, index) => (
-                  <TripDisplay key={index} name={key} value={value} length={value.length} tripdata={tripData} />
-                ))}
+        <>
+          {user ? (
+            <div className={styles.mainContainer}>
+              <div className={styles.topContainer}>
+                <header className={styles.tripHeader}>
+                  <p className={styles.header}>My Trips:</p>
+                  <CreateTrip setModalDisplay={setModalDisplay} />
+                </header>
+                {tripDisplay ? (
+                  <div className={styles.cardContainer}>
+                    {Object.entries(tripData).map(([key, value]: any, index) => (
+                      <TripDisplay key={index} name={key} value={value} length={value.length} tripdata={tripData} />
+                    ))}
+                  </div>
+                ) : (
+                  <TripsDefault setModalDisplay={setModalDisplay} />
+                )}
               </div>
-            ) : (
-              <TripsDefault setModalDisplay={setModalDisplay} />
-            )}
-          </div>
-          {modalDisplay && <Modal setIsOpen={setModalDisplay} />}
-          <div className={styles.bottomContainer}>
-            <button onClick={() => setModalDisplay(true)} className={styles.createTripBtn}>
-              Create Trip
-            </button>
-          </div>
-        </div>
-      ) : (
-        <ProfileDefault />
-      )}
-      <NavBar trips={true} map={false} bucketList={false} profile={false} />
-    </>
-  )
+              {modalDisplay && <Modal setIsOpen={setModalDisplay} />}
+              <div className={styles.bottomContainer}>
+                <button onClick={() => setModalDisplay(true)} className={styles.createTripBtn}>
+                  Create Trip
+                </button>
+              </div>
+            </div>
+          ) : (
+            <ProfileDefault />
+          )}
+          <NavBar trips={true} map={false} bucketList={false} profile={false} />
+        </>
+      )
 }
-
 export default trips
