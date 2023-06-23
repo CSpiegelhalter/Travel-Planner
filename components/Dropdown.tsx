64548bffebd8e40f7c6ac1ae6be0b307d2ccb00d
Brandon Ballesteros <brandonballesteros9845@gmail.com>
2023-06-23 14:44:40 +0100

    saving changes

--- components/Dropdown.tsx
+++ components/Dropdown.tsx
@@ -1,50 +1,46 @@
-import styles from '@/styles/componentStyles/dropdown.module.css'  
+import styles from '@/styles/componentStyles/dropdown.module.css'
 import { useEffect, useState } from 'react'
-  
+
 const Dropdown = (props: any) => {
-    const [showMenu, setShowMenu] = useState(false)
-    const [placeholder, setPlaceholder] =useState('Select a trip!')
-    
-    // useEffect(() => {
-    //     const handler = () => setShowMenu(false)
+  const [showMenu, setShowMenu] = useState(false)
 
-    //     window.addEventListener("click", handler)
-    //     return () => {
-    //         window.removeEventListener('click', handler)
-    //     }
-    // })
+  useEffect(() => {
+      const handler = () => setShowMenu(false)
 
-    const handleInputClick = (e: any) => {
-        e.stopPropagation()
-        setShowMenu(val => !val)
-        
-    }
+      window.addEventListener("click", handler)
+      return () => {
+          window.removeEventListener('click', handler)
+      }
+  })
 
-    const getDisplay = () => {
-    return placeholder
+
+  const handleInputClick = (e: any) => {
+    e.stopPropagation()
+    setShowMenu(!showMenu)
+  }
+
+  const getDisplay = () => {
+    return props.placeholder
   }
 
   return (
     <div className={styles.dropdownContainer}>
       <div className={styles.dropdownInput} onClick={handleInputClick}>
         <div className={styles.dropdownMenu}>
-            { showMenu &&
-            (props.trips.map((trip: any, index: number) => (
-                <div key={index.toString()} className={styles.dropdownItem}>
-                    {trip}
-                </div>
-            )))
-            }
+          {showMenu &&
+            props.trips.map((trip: any, index: number) => (
+              <div key={index.toString()} className={styles.dropdownItem} onClick={handleInputClick}>
+                {trip}
+              </div>
+            ))}
         </div>
         <div className={styles.dropdownSelectedValue}>{getDisplay()}</div>
         <div className={styles.dropdownTools}>
-          <div className={styles.dropdownTool}>
-          </div>
+          <div className={styles.dropdownTool}></div>
         </div>
       </div>
     </div>
   )
 }
-
 
 export default Dropdown
